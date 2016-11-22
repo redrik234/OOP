49a9f14c55175323d7f713ba33d028388e5c5edb
Илларионов Артем Олегович <illarionow.artem@yandex.ru>
2016-11-23 01:06:16 +0300

    Add files via upload

---
+++ flipbyte/flipbyte.cpp
@@ -0,0 +1,53 @@
+// flipbyte.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int flipbyte(int number)
+{
+	int result = 0;
+
+	for (int i = 0; i < 8; i++)
+	{
+		result = (result << 1);
+		if ((number & 1) == 1)
+		{
+			result = result | 1;
+		}
+		number = number >> 1;
+	}
+	return result;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		cout << "Invalid arguments count\n"
+			<< "Usage: flipbyte.exe <input byte>\n";
+		return 1;
+	}
+
+	int num = atoi(argv[1]);
+
+	if ((num == 0) && (*argv[1] != '0'))
+	{
+		cout << "Invalid argument\n";
+		return 1;
+	}
+
+	if ((num < 0) || (num > 255))
+	{
+		cout << "<byte> should be in range [0-255]\n";
+		return 1;
+	}
+
+	cout << flipbyte(num) << endl;
+	return 0;
+
+}
+
+
